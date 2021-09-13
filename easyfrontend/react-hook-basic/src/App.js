5f8896850d5f862c1d0a115094414bf547e24af2
tuannt4 <tuannt4@sapo.vn>
2021-09-13 22:16:56 +0700

    easy: custom hook

--- easyfrontend/react-hook-basic/src/App.js
+++ easyfrontend/react-hook-basic/src/App.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Clock from './components/Clock';
 import BetterClock from './components/BetterClock';
+import MagicBox from './components/MagicBox';
 
 function App() {
   const [showClock, setShowClock] = useState(true);
   return (
     <div className="app">
       <h1>React Hook</h1>
+
       {showClock && <Clock />}
       <BetterClock />
       <button onClick={() => setShowClock(false)}>Hide clock</button>
+
+      <div />
+      <MagicBox />
+
     </div>
   );
 }
