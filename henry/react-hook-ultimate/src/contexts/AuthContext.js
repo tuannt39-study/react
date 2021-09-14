c386eb31fd391994176be1bb4f8ac5c21d9974d5
tuannt4 <tuannt4@sapo.vn>
2021-09-14 10:13:08 +0700

    henry: useEffect

--- henry/react-hook-ultimate/src/contexts/AuthContext.js
+++ henry/react-hook-ultimate/src/contexts/AuthContext.js
@@ -1,9 +1,13 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
 export const AuthContext = createContext()
 
 const AuthContextProvider = ({ children }) => {
   const [isAuthenticated, setAuthentication] = useState(false)
+
+  useEffect(() => {
+    alert(isAuthenticated ? 'Login Successful' : 'You are logged out. Please login to see todos.')
+  }, [isAuthenticated])
 
   const toggleAuth = () => {
     setAuthentication(!isAuthenticated)
